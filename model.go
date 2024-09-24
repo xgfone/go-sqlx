825f2569edc029ebae4041477dfaba4173b3d83a
xgfone <xgfone@126.com>
2024-09-24 17:59:27 +0800

    add the map and string map

--- model.go
+++ model.go
@@ -1,4 +1,4 @@
-// Copyright 2022~2023 xgfone
+// Copyright 2022~2024 xgfone
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,7 +14,14 @@
 
 package sqlx
 
-import "time"
+import (
+	"bytes"
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
+)
 
 const (
 	// DateZero is the ZERO of the sql date.
@@ -35,3 +42,77 @@ type Base struct {
 	UpdatedAt time.Time `sql:"updated_at,omitempty"`
 	DeletedAt time.Time `sql:"deleted_at,omitempty" json:"-"`
 }
+
+// Map is a map value type, which is encoded to a string or decoded from a []byte or string.
+type Map map[string]any
+
+// Value implements the interface driver.Valuer to encode the map to a sql value(string).
+func (m Map) Value() (driver.Value, error) {
+	return encodemap(m)
+}
+
+// Scan implements the interface sql.Scanner to scan a sql value to the map.
+func (m *Map) Scan(src any) error {
+	return decodemap(m, src)
+}
+
+// StringMap is a map value type, which is encoded to a string or decoded from a []byte or string.
+type StringMap map[string]string
+
+// Value implements the interface driver.Valuer to encode the map to a string sql value.
+func (m StringMap) Value() (driver.Value, error) {
+	return encodemap(m)
+}
+
+// Scan implements the interface sql.Scanner to scan a sql value to the map.
+func (m *StringMap) Scan(src any) error {
+	return decodemap(m, src)
+}
+
+// EncodeMap encodes a map to string.
+func EncodeMap[M ~map[string]T, T any](m M) (string, error) {
+	return encodemap(m)
+}
+
+// DecodeMap decodes a map from string or []byte.
+func DecodeMap[M ~map[string]T, T any](m *M, src any) error {
+	return decodemap(m, src)
+}
+
+func encodemap[M ~map[string]T, T any](m M) (s string, err error) {
+	if len(m) == 0 {
+		return
+	}
+
+	buf := bytes.NewBuffer(nil)
+	buf.Grow(64 * len(m))
+	err = json.NewEncoder(buf).Encode(m)
+	s = buf.String()
+	return
+}
+
+func decodemap[M ~map[string]T, T any](m *M, src any) (err error) {
+	switch data := src.(type) {
+	case nil:
+	case []byte:
+		switch {
+		case data == nil, bytes.Equal(data, _jsonbraces), bytes.Equal(data, _jsonnull):
+		default:
+			err = json.NewDecoder(bytes.NewReader(data)).Decode(m)
+		}
+	case string:
+		switch data {
+		case "", "{}", "null":
+		default:
+			err = json.NewDecoder(strings.NewReader(data)).Decode(m)
+		}
+	default:
+		err = fmt.Errorf("converting %T to %T is unsupported", src, *m)
+	}
+	return
+}
+
+var (
+	_jsonbraces = []byte("{}")
+	_jsonnull   = []byte("null")
+)
