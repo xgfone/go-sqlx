961e11bcd509804206eecfdcfe8d0100afee07f5
xgfone <xgfone@126.com>
2025-01-06 11:24:55 +0800

    update the doc comment of deprecated methods

--- dml_select_bind_rows_deprecated.go
+++ dml_select_bind_rows_deprecated.go
@@ -19,17 +19,23 @@ import "context"
 var DefaultSliceCap = 16
 
 // Query builds the sql and executes it.
+//
+// DEPRECATED!!! Please use b.QueryRows() instead.
 func (b *SelectBuilder) Query() (Rows, error) {
 	return b.QueryContext(context.Background())
 }
 
 // QueryContext builds the sql and executes it.
+//
+// DEPRECATED!!! Please use b.QueryRowsContext(ctx) instead.
 func (b *SelectBuilder) QueryContext(ctx context.Context) (Rows, error) {
 	rows := b.QueryRowsContext(ctx)
 	return rows, rows.Err
 }
 
 // BindRows is equal to b.BindRowsContext(context.Background(), slice).
+//
+// DEPRECATED!!! Please use b.QueryRows().Bind(slice) instead.
 func (b *SelectBuilder) BindRows(slice any) error {
 	return b.BindRowsContext(context.Background(), slice)
 }
@@ -39,6 +45,8 @@ func (b *SelectBuilder) BindRows(slice any) error {
 //
 // Notice: slice must be a pointer to a slice. And the element of the slice
 // may be a struct or type implemented the interface sql.Scanner.
+//
+// DEPRECATED!!! Please use b.QueryRowsContext(ctx).Bind(slice) instead.
 func (b *SelectBuilder) BindRowsContext(ctx context.Context, slice any) error {
 	rows, err := b.QueryContext(ctx)
 	return rows.TryBindSlice(slice, err)
@@ -46,6 +54,8 @@ func (b *SelectBuilder) BindRowsContext(ctx context.Context, slice any) error {
 
 // TryBindSlice is the same as BindSlice, which binds rows to slice
 // only if err is equal to nil.
+//
+// DEPRECATED!!! Please use r.Bind(slice) instead.
 func (r Rows) TryBindSlice(slice any, err error) error {
 	if err != nil {
 		return err
@@ -54,6 +64,8 @@ func (r Rows) TryBindSlice(slice any, err error) error {
 }
 
 // BindSlice is the same as ScanSlice, but closes sql.Rows.
+//
+// DEPRECATED!!! Please use r.Bind(slice) instead.
 func (r Rows) BindSlice(slice any) (err error) {
 	defer r.Rows.Close()
 	return r.ScanSlice(slice)
@@ -61,6 +73,8 @@ func (r Rows) BindSlice(slice any) (err error) {
 
 // ScanStruct is the same as Scan, but the columns are scanned into the struct
 // s, which uses ScanColumnsToStruct.
+//
+// DEPRECATED!!! Please use r.Bind(slice) instead.
 func (r Rows) ScanStruct(s any) (err error) {
 	if r.Rows == nil {
 		return
@@ -70,12 +84,16 @@ func (r Rows) ScanStruct(s any) (err error) {
 
 // ScanStructWithColumns is the same as Scan, but the columns are scanned
 // into the struct s by using ScanColumnsToStruct.
+//
+// DEPRECATED!!! Please use r.Bind(slice) instead.
 func (r Rows) ScanStructWithColumns(s any, columns ...string) (err error) {
 	r.columns = columns
 	return r.ScanStruct(s)
 }
 
 // ScanSlice is used to scan the row set into the slice.
+//
+// DEPRECATED!!! Please use r.Bind(slice) instead.
 func (r Rows) ScanSlice(slice any) (err error) {
 	return CommonSliceRowsBinder.BindRows(r, slice)
 }
